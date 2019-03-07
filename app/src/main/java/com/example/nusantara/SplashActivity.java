821c9b366858a1889db7acfb13b4754ee55e65a2
fahruldatu <fahruldatu12@gmail.com>
2019-03-07 21:38:30 +0800

    Menambahkan Splash Screen

---
+++ app/src/main/java/com/example/nusantara/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.nusantara;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        //HIDE STATUS BAR
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
