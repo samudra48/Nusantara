c8f89580bd27a15d677d834391056f0d06a05647
bakiatussrfh <bakiatussaridon@gmail.com>
2019-03-18 18:57:44 +0700

    ini tinggal di hubungin ke firebase teman2....

--- app/src/main/java/com/example/nusantara/Registrasi.java
+++ app/src/main/java/com/example/nusantara/Registrasi.java
@@ -1,78 +1,138 @@
 package com.example.nusantara;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-import modal.User;
-import sql.DatabaseHelper;
+import java.util.Date;
 
-public class Registrasi extends AppCompatActivity {
-    private static final String TAG_ACTIVITY = Registrasi.class.getSimpleName();
-
-    TextView textView12;
-    TextView textView13;
-    TextView textView_name;
-    TextView textView_email;
-    TextView textView_psw;
-    TextView kp;
-
-
-    TextView textView_login;
-    EditText editText_name;
-    EditText editText_email;
-    EditText editText_kp;
-    EditText editText_psw;
-    Button register;
-
-
+public class Registrasi extends AppCompatActivity implements View.OnClickListener{
+    EditText name,email,password;
+    Button mRegisterbtn;
+    TextView mLoginPageBack;
+    FirebaseAuth mAuth;
+    DatabaseReference mdatabase;
+    String Name,Email,Password;
+    ProgressDialog mDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrasi);
-        getSupportActionBar().hide();
-
-        initView();
-        initListener();
-
-
-
-    }
-
-    private void initListener() {
-        register.setOnClickListener((View.OnClickListener) this);
-    }
-
-
-    private void initView() {
-        textView12 = (TextView) findViewById(R.id.textView12);
-        textView_name = (TextView) findViewById(R.id.textView_name);
-        textView_email = (TextView) findViewById(R.id.textView_email);
-        textView_psw = (TextView) findViewById(R.id.textView_psw);
-        kp = (TextView) findViewById(R.id.kp);
-        textView13 = (TextView) findViewById(R.id.textView13);
-        textView_login = (TextView) findViewById(R.id.textView_login);
-        editText_name = (EditText) findViewById(R.id.editText_name);
-        editText_email = (EditText) findViewById(R.id.editText_email);
-        editText_kp = (EditText) findViewById(R.id.editText_kp);
-        editText_psw = (EditText) findViewById(R.id.editText_psw);
-        register = (Button) findViewById(R.id.register);
-
+        name = (EditText)findViewById(R.id.editName);
+        email = (EditText)findViewById(R.id.editEmail);
+        password = (EditText)findViewById(R.id.editPassword);
+        mRegisterbtn = (Button)findViewById(R.id.buttonRegister);
+        mLoginPageBack = (TextView)findViewById(R.id.buttonLogin);
+        // for authentication using FirebaseAuth.
+        mAuth = FirebaseAuth.getInstance();
+        mRegisterbtn.setOnClickListener(this);
+        mLoginPageBack.setOnClickListener(this);
+        mDialog = new ProgressDialog(this);
+        mdatabase = FirebaseDatabase.getInstance().getReference().child("Users");
 
     }
 
+    @Override
+    public void onClick(View v) {
+        if (v==mRegisterbtn){
+            UserRegister();
+        }else if (v== mLoginPageBack){
+            startActivity(new Intent(Registrasi.this,MainActivity.class));
+        }
+    }
 
+    private void UserRegister() {
+        Name = name.getText().toString().trim();
+        Email = email.getText().toString().trim();
+        Password = password.getText().toString().trim();
 
+        if (TextUtils.isEmpty(Name)){
+            Toast.makeText(Registrasi.this, "Enter Name", Toast.LENGTH_SHORT).show();
+            return;
+        }else if (TextUtils.isEmpty(Email)){
+            Toast.makeText(Registrasi.this, "Enter Email", Toast.LENGTH_SHORT).show();
+            return;
+        }else if (TextUtils.isEmpty(Password)){
+            Toast.makeText(Registrasi.this, "Enter Password", Toast.LENGTH_SHORT).show();
+            return;
+        }else if (Password.length()<6){
+            Toast.makeText(Registrasi.this,"Passwor must be greater then 6 digit",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        mDialog.setMessage("Creating User please wait...");
+        mDialog.setCanceledOnTouchOutside(false);
+        mDialog.show();
+        mAuth.createUserWithEmailAndPassword(Email,Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    sendEmailVerification();
+                    mDialog.dismiss();
+                    OnAuth(task.getResult().getUser());
+                    mAuth.signOut();
+                }else{
+                    Toast.makeText(Registrasi.this,"error on creating user",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    //Email verification code using FirebaseUser object and using isSucccessful()function.
+    private void sendEmailVerification() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user!=null){
+            user.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()){
+                        Toast.makeText(Registrasi.this,"Check your Email for verification",Toast.LENGTH_SHORT).show();
+                        FirebaseAuth.getInstance().signOut();
+                    }
+                }
+            });
+        }
+    }
+
+    private void OnAuth(FirebaseUser user) {
+        createAnewUser(user.getUid());
+    }
+
+    private void createAnewUser(String uid) {
+        User user = BuildNewuser();
+        mdatabase.child(uid).setValue(user);
     }
 
 
+    private User BuildNewuser(){
+        return new User(
+                getDisplayName(),
+                getUserEmail(),
+                new Date().getTime()
+        );
+    }
 
+    public String getDisplayName() {
+        return Name;
+    }
+
+    public String getUserEmail() {
+        return Email;
+    }
+
+}
