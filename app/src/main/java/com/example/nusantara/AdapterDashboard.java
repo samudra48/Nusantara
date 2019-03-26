e611126115c162c1e187603ce12eeade2a7729b3
Rowitagirsang <girsangrowita@gmail.com>
2019-03-26 20:28:21 +0700

    commit

--- app/src/main/java/com/example/nusantara/AdapterDashboard.java
+++ app/src/main/java/com/example/nusantara/AdapterDashboard.java
@@ -3,6 +3,7 @@ package com.example.nusantara;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 public class AdapterDashboard extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
@@ -11,7 +12,7 @@ public class AdapterDashboard extends RecyclerView.Adapter<RecyclerView.ViewHold
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return new .ViewHolder(LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_dashboard,viewGroup,false));
+        return new ViewHolder(LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_dashboard,viewGroup,false));
     }
 
     @Override
@@ -22,5 +23,12 @@ public class AdapterDashboard extends RecyclerView.Adapter<RecyclerView.ViewHold
     @Override
     public int getItemCount() {
         return 0;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
     }
 }
