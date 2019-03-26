2272f37acc187391bd98e255a6c2ba5c6460300f
bakiatussrfh <bakiatussaridon@gmail.com>
2019-03-26 20:16:37 +0700

    coyyy

---
+++ app/src/main/java/com/example/nusantara/AdapterDashboard.java
@@ -0,0 +1,26 @@
+package com.example.nusantara;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+public class AdapterDashboard extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return new .ViewHolder(LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_dashboard,viewGroup,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
