4924880ee72763f98c402f825cdce7491c0dbe92
IkkiDaiten <rizqiemf202gmail.com>
2019-04-14 17:35:57 +0700

    sprint 3 start

--- app/src/main/java/com/example/nusantara/LoginAdminActivity.java
+++ app/src/main/java/com/example/nusantara/LoginAdminActivity.java
@@ -33,7 +33,8 @@ public class LoginAdminActivity extends AppCompatActivity {
     }
     private void validate(String username,String password){
         if ((username == "Nusantara") && (password == "NusantaraApps")){
-
+            Intent intent = new Intent(LoginAdminActivity.this,AdminDashboardActivity.class);
+            startActivity(intent);
         }
         else {
             counter--;
