baa4f734495d04f243bd9fe6bd5160ee6286a595
rizqiemf <rizqiemf@users.noreply.github.com>
2019-04-25 13:49:49 +0700

    ini buat admin

--- app/src/main/java/com/example/nusantara/AdminDashboardActivity.java
+++ app/src/main/java/com/example/nusantara/AdminDashboardActivity.java
@@ -1,10 +1,13 @@
 package com.example.nusantara;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatDelegate;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class AdminDashboardActivity extends AppCompatActivity {
     final String PREF_NIGHT_MODE = "NightMode";
@@ -27,4 +30,21 @@ public class AdminDashboardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_dashboard);
     }
-}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.admin_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.option_logoutadmin:
+                startActivity(new Intent(getBaseContext(),LoginAdminActivity.class));
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    }
--- app/src/main/java/com/example/nusantara/TampilanFotoUploadan.java
+++ app/src/main/java/com/example/nusantara/TampilanFotoUploadan.java
@@ -2,15 +2,39 @@ package com.example.nusantara;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class TampilanFotoUploadan extends AppCompatActivity {
+
+    ImageView img_uplodan;
+    TextView tv_captionuploadan, tv_captionlain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tampilan_foto_uploadan);
 
+        img_uplodan= findViewById(R.id.img_upload);
+        tv_captionuploadan= findViewById(R.id.tv_captionan );
+        tv_captionlain = findViewById(R.id.tv_capsionlain);
 
+        if (getIntent() != null){
+            String nameA = getIntent().getStringExtra("nama");
+            String descB = getIntent().getStringExtra("deskripsi");
+            String gambarC = getIntent().getStringExtra("gambar");
 
+            if (!nameA.isEmpty()){
+                tv_captionlain.setText(nameA);
+            }
+            if (!descB.isEmpty()){
+                tv_captionuploadan.setText(descB);
+            }
+            if (!gambarC.isEmpty()){
+                Picasso.get().load(gambarC).into(img_uplodan);
+            }
+        }
     }
 }
